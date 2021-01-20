de48621d7f1d8ea523efca4ecebe7c15303412e0
AdinGRT <adingrt@gmail.com>
2021-01-20 15:05:51 -0600

    se creo clase de domain

---
+++ src/peliculas/domain/Peliculas.java
@@ -0,0 +1,26 @@
+package peliculas.domain;
+
+/**
+ *
+ * @author adingrt
+ */
+public class Peliculas {
+    private String nombre;
+
+    public Peliculas() {
+    }
+
+    public Peliculas(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
