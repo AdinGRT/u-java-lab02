5de010074f8e4f8a65c6ed514f70d05e5c67cff8
AdinGRT <adingrt@gmail.com>
2021-01-25 12:00:37 -0600

    se creo clase AccesoDatosImpl con metodos existe() y listar()

---
+++ src/peliculas/datos/AccesoDatosImpl.java
@@ -0,0 +1,60 @@
+package peliculas.datos;
+
+import java.io.*;
+import java.util.*;
+import peliculas.domain.Pelicula;
+import peliculas.excepciones.*;
+
+/**
+ *
+ * @author adingrt
+ */
+public class AccesoDatosImpl implements IAccesoDatos {
+
+    @Override
+    public boolean existe(String nombreRecurso) throws AccesoDatosEx {
+        var archivo = new File(nombreRecurso);
+        return archivo.exists();
+    }
+
+    @Override
+    public List<Pelicula> listar(String nombreRecurso) throws LecturaDatosEx {
+        var archivo = new File(nombreRecurso);
+        List<Pelicula> peliculas = new ArrayList<>();
+        try {
+            var entrada = new BufferedReader(new FileReader(archivo));
+            String linea = null;
+            linea = entrada.readLine();
+            while (linea != null) {
+                var pelicula = new Pelicula(linea);
+                peliculas.add(pelicula);
+                linea = entrada.readLine();
+            }
+            
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas: " + ex.getMessage());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            throw new LecturaDatosEx("Excepcion al listar peliculas: " + ex.getMessage());
+        }
+        return peliculas;
+    }
+
+    @Override
+    public void escribir(Pelicula pelicula, String nombreRecurso, boolean anexar) throws EscrituraDatosEx {
+    }
+
+    @Override
+    public String buscar(String nombreRecurso, String buscar) throws LecturaDatosEx {
+    }
+
+    @Override
+    public void crear(String nombreRecurso) throws AccesoDatosEx {
+    }
+
+    @Override
+    public void borrar(String nombreRecurso) throws AccesoDatosEx {
+    }
+    
+}
